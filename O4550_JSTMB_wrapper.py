0c0a91f37473c60d8347bfeabae7c25bdfc593ed
ISL2017-RPI <31634008+ISL2017-RPI@users.noreply.github.com>
2017-09-08 15:47:40 -0400

    Update O4550_JSTMB_wrapper.py

--- O4550_JSTMB_wrapper.py
+++ O4550_JSTMB_wrapper.py
@@ -2,7 +2,7 @@ import sys
 import JSTMB_O4550
 import numpy as np
 
-def JSTMB(data_file = 'trainData.csv', target_file = 'trainargets.csv'):
+def JSTMB(data_file = 'trainData.csv', target_file = 'trainTargets.csv'):
     my_JMI = JSTMB_O4550.initialize()
     feat = my_JMI.JSTMB_primitive_O4550(data_file, target_file)
     return feat
